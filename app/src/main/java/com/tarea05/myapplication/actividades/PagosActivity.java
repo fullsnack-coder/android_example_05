232efcdcc7be8f13d5fc48f06848085f80e31888
Manuel Garcia <manueldev250799@gmail.com>
2023-06-08 16:23:30 -0500

    feat: add activity pagos

---
+++ app/src/main/java/com/tarea05/myapplication/actividades/PagosActivity.java
@@ -0,0 +1,19 @@
+package com.tarea05.myapplication.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.tarea05.myapplication.R;
+
+public class PagosActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagos);
+
+    }
+}
